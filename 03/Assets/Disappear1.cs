7fd16981ab06a435531cd02878fcd91a351df0bf
Katanasouls <jak_emp@hotmail.com>
2019-03-12 21:23:26 +0700

    Add
    Yeah

---
+++ 03/Assets/Disappear1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            this.transform.position = this.transform.position + new Vector3 (0, 0, -1);
+        }
+        if (Input.GetKeyDown("u"))
+        {
+            this.transform.position = this.transform.position + new Vector3 (0, 0, 1);
+        }
+    }
+}
---
+++ 03/Assets/HallwayRandom.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HallwayRandom : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] doorControl;
+    // Start is called before the first frame update
+    public Vector3 doorInit1, doorInit2, doorInit3, doorInit4;
+    void Start()
+    {
+       // doorInit1 = doorControl[0].transform.position;
+        doorInit2 = doorControl[1].transform.position;
+        doorInit3 = doorControl[2].transform.position;
+        doorInit4 = doorControl[3].transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            int max = 0;
+            doorControl[0].transform.position = doorInit1;
+            doorControl[1].transform.position = doorInit2;
+            doorControl[2].transform.position = doorInit3;
+            doorControl[3].transform.position = doorInit4;
+            while (true)
+            {
+                max = 0;
+                doorControl[0].transform.position = doorInit1;
+                doorControl[1].transform.position = doorInit2;
+                doorControl[2].transform.position = doorInit3;
+                doorControl[3].transform.position = doorInit4;
+                if (Random.value > 0.5)
+                {
+                    doorControl[0].transform.position = doorControl[0].transform.position + new Vector3(0, -5, 0);
+                    max++;
+                }
+                if (Random.value > 0.5)
+                {
+                    doorControl[1].transform.position = doorControl[1].transform.position + new Vector3(0, -5, 0);
+                    max++;
+                }
+                if (Random.value > 0.5)
+                {
+                    doorControl[2].transform.position = doorControl[2].transform.position + new Vector3(0, -5, 0);
+                    max++;
+                }
+                if (Random.value > 0.5)
+                {
+                    doorControl[3].transform.position = doorControl[3].transform.position + new Vector3(0, -5, 0);
+                    max++;
+                }
+                //if (max > 2)
+               // {
+                    break;
+               // }
+            }
+        }
+    }
+}
